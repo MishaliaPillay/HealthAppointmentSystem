b2f35e4251b520e1fc561c2935c7c07ce93ec535
KatlehoMadaba <101579604+KatlehoMadaba@users.noreply.github.com>
2025-04-11 09:24:57 +0200

    Updated mappings correctly with the required ignores

---
+++ Backend/HealthAPP/9.4.2/aspnet-core/src/HealthAPP.Application/Services/PersonService/Mapping/CustomDtoMappings.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using HealthAPP.Domain.Appointments;
+using HealthAPP.Domain.Persons;
+using HealthAPP.Services.PersonService.Dto;
+
+namespace HealthAPP.Services.PersonService.Mapping
+{
+    class CustomDtoMappings: Profile
+    {
+        public CustomDtoMappings()
+        {
+            CreateMap<Person, PersonResponseDto>();
+
+            CreateMap<Patient, PatientResponseDto>();
+            CreateMap<PatientRequestDto, Patient>()
+                .ForMember(d => d.Id, opt => opt.Ignore())
+                .ForMember(d => d.User, opt => opt.Ignore())
+                .ForMember(d => d.UserId, opt => opt.Ignore());
+
+            CreateMap<Provider, ProviderResponseDto>();
+            CreateMap<ProviderRequestDto, Provider>()
+                .ForMember(d => d.Id, opt => opt.Ignore())
+                .ForMember(d => d.User, opt => opt.Ignore())
+                .ForMember(d => d.UserId, opt => opt.Ignore());
+            CreateMap<Appointment, AppointmentDto>();
+            CreateMap<ProviderAvailabilty, ProviderAvailabiltyDto>();
+
+        }
+
+
+    }
+}
