1ef607ee42a8b5629a443343b2ee27f7975a3500
KatlehoMadaba <101579604+KatlehoMadaba@users.noreply.github.com>
2025-04-11 17:08:20 +0200

    Added Title missing property

--- Backend/healthap/10.0.1/aspnet-core/src/healthap.Application/Services/PersonServices/PatientAppService.cs
+++ Backend/healthap/10.0.1/aspnet-core/src/healthap.Application/Services/PersonServices/PatientAppService.cs
@@ -24,6 +24,7 @@ namespace healthap.Services.PersonServices
         {
 
             var patient = await _patientManager.CreatePatientAsync(
+                input.Title,
                 input.FirstName,
                 input.Surname,
                 input.Email,
