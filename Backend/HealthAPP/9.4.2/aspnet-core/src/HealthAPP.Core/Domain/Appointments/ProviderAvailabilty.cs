cbcafd69b228890ce05552ab083f76f4a8222c33
KatlehoMadaba <101579604+KatlehoMadaba@users.noreply.github.com>
2025-04-11 13:01:07 +0200

    Updated with required enities soon to add required entites

--- Backend/HealthAPP/9.4.2/aspnet-core/src/HealthAPP.Core/Domain/Appointments/ProviderAvailabilty.cs
+++ Backend/HealthAPP/9.4.2/aspnet-core/src/HealthAPP.Core/Domain/Appointments/ProviderAvailabilty.cs
@@ -1,12 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Abp.Domain.Entities.Auditing;
+using HealthAPP.Domain.Persons;
 
 namespace HealthAPP.Domain.Appointments
 {
-    public class ProviderAvailabilty
+
+    public class ProviderAvailabilty:FullAuditedEntity
     {
+
+        //DayOfWeek:DayOfWeek
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan EndTime { get; set; }
+        public bool IsAvailable { get; set; }
+        public virtual Provider Provider { get; set; }
     }
 }
