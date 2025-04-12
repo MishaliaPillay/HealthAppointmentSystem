bdd649c32bd8e9d8ba1c348920713446c64134b9
Tondwani <craigmangaladzi@gmail.com>
2025-04-13 00:50:25 +0200

    Implemented the mapping on the location entity

--- Backend/healthap/10.0.1/aspnet-core/src/healthap.EntityFrameworkCore/EntityFrameworkCore/healthapDbContext.cs
+++ Backend/healthap/10.0.1/aspnet-core/src/healthap.EntityFrameworkCore/EntityFrameworkCore/healthapDbContext.cs
@@ -1,6 +1,9 @@
 ﻿using Abp.Zero.EntityFrameworkCore;
 using healthap.Authorization.Roles;
 using healthap.Authorization.Users;
+using healthap.Domain.Appointments;
+using healthap.Domain.Location;
+using healthap.Domain.Persons;
 using healthap.MultiTenancy;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,11 +12,31 @@ namespace healthap.EntityFrameworkCore;
 public class healthapDbContext : AbpZeroDbContext<Tenant, Role, User, healthapDbContext>
 {
     /* Define a DbSet for each entity of the application */
+    public DbSet<Patient> Patients { get; set; }
+    public DbSet<Provider> Providers { get; set; }
     public DbSet<Appointment> Appointments { get; set; }
+    public DbSet<Location> Location { get; set; }
+    public DbSet<ProviderAvailabilty> ProviderAvailabilities { get; set; }
 
     public healthapDbContext(DbContextOptions<healthapDbContext> options)
         : base(options)
     {
     }
 
+    protected void ConfigureLocation(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Location>(entity =>
+        {
+            entity.ToTable("Locations");
+            entity.HasIndex(e => new { e.Latitude, e.Longitude });
+
+            entity.Property(e => e.Address).IsRequired().HasMaxLength(100);
+            entity.Property(e => e.City).IsRequired().HasMaxLength(100);
+            entity.Property(e => e.State).HasMaxLength(100);
+            entity.Property(e => e.PostalCode).HasMaxLength(30);
+            entity.Property(e => e.Country).IsRequired().HasMaxLength(100);
+            entity.Property(e => e.Description).HasMaxLength(255);
+        });
+
+    }
 }
