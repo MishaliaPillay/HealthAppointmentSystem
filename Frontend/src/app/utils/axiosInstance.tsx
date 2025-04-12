2dcad9e223f3a4f84dd509711ed135c23be20bcc
KatlehoMadaba <101579604+KatlehoMadaba@users.noreply.github.com>
2025-04-12 14:22:35 +0200

    used prettier to fix code format

--- Frontend/src/app/utils/axiosInstance.tsx
+++ Frontend/src/app/utils/axiosInstance.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const getAxiosInstace = () =>
   axios.create({
-    baseURL: `${'must have the base url here once deployed'}`,
+    baseURL: `${"must have the base url here once deployed"}`,
     headers: {
       "Content-Type": "application/json",
     },
--- Frontend/src/providers/paitient-provider/actions.tsx
+++ Frontend/src/providers/paitient-provider/actions.tsx
@@ -1,32 +1,30 @@
-"use client"
-import { IPatient } from './models';
-import {IPatientStateContext } from './context';
-import {createAction} from "redux-actions";
+"use client";
+import { IPatient } from "./models";
+import { IPatientStateContext } from "./context";
+import { createAction } from "redux-actions";
 
 export enum PatientActionEnums {
+  getPatientsPending = "GET_PATIENTS_PENDING",
+  getPatientsSuccess = "GET_PATIENTS_SUCCESS",
+  getPatientsError = "GET_PATIENTS_ERROR",
 
-    getPatientsPending = "GET_PATIENTS_PENDING",
-    getPatientsSuccess = "GET_PATIENTS_SUCCESS",
-    getPatientsError = "GET_PATIENTS_ERROR",
-  
-    getPatientPending = "GET_PATIENT_PENDING",
-    getPatientSuccess = "GET_PATIENT_SUCCESS",
-    getPatientError = "GET_PATIENT_ERROR",
-  
-   registerPatientPending = "CREATE_PATIENT_PENDING",
-   registerPatientSuccess = "CREATE_PATIENT_SUCCESS",
-   registerPatientError = "CREATE_PATIENT_ERROR",
-  
-    updatePatientPending = "UPDATE_=PATIENT_PENDING",
-    updatePatientSuccess = "UPDATE_=PATIENT_SUCCESS",
-    updatePatientError = "UPDATE_=PATIENT_ERROR",
-  
-    deletePatientPending = "DELETE_PATIENT_PENDING",
-    deletePatientSuccess = "DELETE_PATIENT_SUCCESS",
-    deletePatientError = "DELETE_PATIENT_ERROR",
-  }
+  getPatientPending = "GET_PATIENT_PENDING",
+  getPatientSuccess = "GET_PATIENT_SUCCESS",
+  getPatientError = "GET_PATIENT_ERROR",
 
-  
+  registerPatientPending = "CREATE_PATIENT_PENDING",
+  registerPatientSuccess = "CREATE_PATIENT_SUCCESS",
+  registerPatientError = "CREATE_PATIENT_ERROR",
+
+  updatePatientPending = "UPDATE_=PATIENT_PENDING",
+  updatePatientSuccess = "UPDATE_=PATIENT_SUCCESS",
+  updatePatientError = "UPDATE_=PATIENT_ERROR",
+
+  deletePatientPending = "DELETE_PATIENT_PENDING",
+  deletePatientSuccess = "DELETE_PATIENT_SUCCESS",
+  deletePatientError = "DELETE_PATIENT_ERROR",
+}
+
 //Get All Paitients
 //Multiple Paitients
 export const getPatientsPending = createAction<IPatientStateContext>(
@@ -34,17 +32,14 @@ export const getPatientsPending = createAction<IPatientStateContext>(
   () => ({ isPending: true, isSuccess: false, isError: false })
 );
 export const getPatientsSuccess = createAction<
-  IPatientStateContext, 
-  IPatient[]           
->(
-  PatientActionEnums.getPatientsSuccess,
-  (Patients: IPatient[]) => ({
-    isPending: false,
-    isSuccess: true,
-    isError: false,
-    Patients, 
-  })
-);
+  IPatientStateContext,
+  IPatient[]
+>(PatientActionEnums.getPatientsSuccess, (Patients: IPatient[]) => ({
+  isPending: false,
+  isSuccess: true,
+  isError: false,
+  Patients,
+}));
 
 export const getPatientsError = createAction<IPatientStateContext>(
   PatientActionEnums.getPatientsError,
@@ -72,26 +67,26 @@ export const getPatientSuccess = createAction<IPatientStateContext, IPatient>(
   })
 );
 
-  //Registering The Patient
-  export const registerPatientPending = createAction<IPatientStateContext>(
-    PatientActionEnums.registerPatientPending,
-    () => ({ isPending: true, isSuccess: false, isError: false })
-  );
-  
-  export const registerPatientSuccess = createAction<
-    IPatientStateContext,
-    IPatient
-  >(PatientActionEnums.registerPatientSuccess, (Patient: IPatient) => ({
-    isPending: false,
-    isSuccess: true,
-    isError: false,
-    Patient,
-  }));
+//Registering The Patient
+export const registerPatientPending = createAction<IPatientStateContext>(
+  PatientActionEnums.registerPatientPending,
+  () => ({ isPending: true, isSuccess: false, isError: false })
+);
 
-  export const registerPatientError = createAction<IPatientStateContext>(
-    PatientActionEnums.registerPatientError,
-    () => ({ isPending: false, isSuccess: false, isError: true })
-  );
+export const registerPatientSuccess = createAction<
+  IPatientStateContext,
+  IPatient
+>(PatientActionEnums.registerPatientSuccess, (Patient: IPatient) => ({
+  isPending: false,
+  isSuccess: true,
+  isError: false,
+  Patient,
+}));
+
+export const registerPatientError = createAction<IPatientStateContext>(
+  PatientActionEnums.registerPatientError,
+  () => ({ isPending: false, isSuccess: false, isError: true })
+);
 
 //Update The Patient
 export const updatePatientPending = createAction<IPatientStateContext>(
@@ -115,23 +110,22 @@ export const updatePatientError = createAction<IPatientStateContext>(
 );
 
 //Delete Patient
-  export const deletePatientPending = createAction<IPatientStateContext>(
-    PatientActionEnums.deletePatientPending,
-    () => ({ isPending: true, isSuccess: false, isError: false })
-  );
-  
-  export const deletePatientSuccess = createAction<
-    IPatientStateContext,
-    IPatient
-  >(PatientActionEnums.deletePatientSuccess, (Patient: IPatient) => ({
-    isPending: false,
-    isSuccess: true,
-    isError: false,
-    Patient,
-  }));
-  
-  export const deletePatientError = createAction<IPatientStateContext>(
-    PatientActionEnums.deletePatientError,
-    () => ({ isPending: false, isSuccess: false, isError: true })
-  );
+export const deletePatientPending = createAction<IPatientStateContext>(
+  PatientActionEnums.deletePatientPending,
+  () => ({ isPending: true, isSuccess: false, isError: false })
+);
 
+export const deletePatientSuccess = createAction<
+  IPatientStateContext,
+  IPatient
+>(PatientActionEnums.deletePatientSuccess, (Patient: IPatient) => ({
+  isPending: false,
+  isSuccess: true,
+  isError: false,
+  Patient,
+}));
+
+export const deletePatientError = createAction<IPatientStateContext>(
+  PatientActionEnums.deletePatientError,
+  () => ({ isPending: false, isSuccess: false, isError: true })
+);
