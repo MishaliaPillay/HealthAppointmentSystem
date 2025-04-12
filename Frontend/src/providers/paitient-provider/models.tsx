e36114cdb392209554038ada953eded2671a3382
KatlehoMadaba <101579604+KatlehoMadaba@users.noreply.github.com>
2025-04-12 09:20:52 +0200

    created the properties

---
+++ Frontend/src/providers/paitient-provider/models.tsx
@@ -0,0 +1,29 @@
+ export interface IPaitientRegistration{
+    FirstName:string;
+    LastName:string ;
+    Email:string ;
+    PhoneNumbe:string;
+    Role :string;
+    DateOfBirth:Date;
+    Address:string ;
+    City :string;
+    Province :string;
+    PostalCode:string;
+    Country :string;
+ }
+export interface IPaitient {
+    _id:string;
+    FirstName:string;
+    LastName:string ;
+    Email:string ;
+    PhoneNumbe:string;
+    Role :string;
+    DateOfBirth:Date;
+    Address:string 
+    City :string;
+    Province :string;
+    PostalCode:string;
+    Country :string;
+}
+
+
