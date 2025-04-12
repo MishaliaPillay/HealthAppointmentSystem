25b1a4763762f98745164b274bc5fa35d5172a32
KatlehoMadaba <101579604+KatlehoMadaba@users.noreply.github.com>
2025-04-12 22:55:58 +0200

    updated with required provider information and created basic cruds

---
+++ Frontend/src/providers/providerMedicPrac-provider/index.tsx
@@ -0,0 +1,155 @@
+import { getAxiosInstace } from "../../app/utils/axiosInstance";
+import { IProvider } from "./models";
+import {
+  INITIAL_STATE,
+  ProviderActionContext,
+  ProviderStateContext,
+} from "./context";
+import { ProviderReducer } from "./reducer";
+import { useContext, useReducer } from "react";
+import {
+  registerProviderPending,
+  registerProviderError,
+  registerProviderSuccess,
+  getProviderPending,
+  getProvidersPending,
+  getProvidersSuccess,
+  getProvidersError,
+  getProviderError,
+  updateProviderPending,
+  updateProviderSuccess,
+  updateProviderError,
+  deleteProviderPending,
+  deleteProviderSuccess,
+} from "./actions";
+
+export const ProviderProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [state, dispatch] = useReducer(ProviderReducer, INITIAL_STATE);
+  const instance = getAxiosInstace();
+
+  //Register the Provider
+  const registerProvider = async (Provider: IProvider) => {
+    dispatch(registerProviderPending());
+    const endpoint = `/register`;
+    await instance
+      .post(endpoint, Provider)
+      .then((response) => {
+        dispatch(registerProviderSuccess(response.data));
+      })
+      .catch((error) => {
+        console.error(error);
+        dispatch(registerProviderError());
+      })
+      .finally(() => {
+        console.log("Register user complete");
+      });
+  };
+
+  //Get All Providers
+  const getProviders = async () => {
+    dispatch(getProvidersPending());
+    const endpoint = `providers`;
+    await instance
+      .post(endpoint)
+      .then((response) => {
+        dispatch(getProvidersSuccess(response.data));
+      })
+      .catch((error) => {
+        console.error(error);
+        dispatch(getProvidersError());
+      })
+      .finally(() => {
+        console.log("getProviders user complete");
+      });
+  };
+
+  //Get Provider
+  const getProvider = async (ProviderId: string) => {
+    dispatch(getProviderPending());
+    const endpoint = `Provider`;
+    await instance
+      .post(endpoint, ProviderId)
+      .then((response) => {
+        dispatch(getProvidersSuccess(response.data));
+      })
+      .catch((error) => {
+        console.error(error);
+        dispatch(getProviderError());
+      })
+      .finally(() => {
+        console.log("getProvider user complete");
+      });
+  };
+
+  //Update Paitient
+  const updateProvider = async (Provider: IProvider) => {
+    dispatch(updateProviderPending());
+    const endpoint = `updateProvider`;
+    await instance
+      .post(endpoint, Provider)
+      .then((response) => {
+        dispatch(updateProviderSuccess(response.data));
+      })
+      .catch((error) => {
+        console.error(error);
+        dispatch(updateProviderError());
+      })
+      .finally(() => {
+        console.log("updateProvider user complete");
+      });
+  };
+
+  //Delete Provider
+  const deleteProviderbyId = async (ProviderId: string) => {
+    dispatch(deleteProviderPending());
+    const endpoint = `${ProviderId}`;
+    await instance
+      .delete(endpoint)
+      .then((response) => {
+        dispatch(deleteProviderSuccess(response.data));
+      })
+      .catch((error) => {
+        dispatch(deleteProviderSuccess(error.data));
+      })
+      .finally(() => {
+        console.log("delete user complete");
+      });
+  };
+
+  return (
+    <ProviderStateContext.Provider value={state}>
+      <ProviderActionContext.Provider
+        value={{
+          registerProvider,
+          getProviders,
+          getProvider,
+          updateProvider,
+          deleteProviderbyId,
+        }}
+      >
+        {children}
+      </ProviderActionContext.Provider>
+    </ProviderStateContext.Provider>
+  );
+};
+export const useProviderState = () => {
+  const context = useContext(ProviderStateContext);
+  if (!context) {
+    throw new Error("useProviderState must be used within a ProviderProvider");
+  }
+  return context;
+};
+
+export const useProviderActions = () => {
+  const context = useContext(ProviderActionContext);
+  if (!context) {
+    throw new Error(
+      "ProviderActionContext must be used within a ProviderProvider"
+    );
+  }
+  return context;
+};
