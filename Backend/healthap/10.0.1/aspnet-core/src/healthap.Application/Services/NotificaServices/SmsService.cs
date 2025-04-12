669691d4ab377eac2a3a4838611122f1ec611d09
MatimaMk <matima547@gmail.com>
2025-04-12 21:51:16 +0200

    smsIntegration

---
+++ Backend/healthap/10.0.1/aspnet-core/src/healthap.Application/Services/NotificaServices/SmsService.cs
@@ -0,0 +1,27 @@
+﻿using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace healthap.Services.NotificaServices
+{
+    public class SmsService
+    {
+        public static void SendMessage(string cell, string msg)
+        {
+            string accountSid = "AC5c62df0d54b603eb0e268a03d3188502";
+            string authToken = "08bc46f5489c650da3f9e8914976125f";
+            string fromNumber = "+15807413538";
+
+
+
+            TwilioClient.Init(accountSid, authToken);
+
+            var message = MessageResource.Create(
+                body: msg,
+                from: new Twilio.Types.PhoneNumber("+15807413538"),
+                to: new Twilio.Types.PhoneNumber(cell)
+
+                );
+        }
+
+    }
+}
