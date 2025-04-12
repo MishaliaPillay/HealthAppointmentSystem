69eec4e018443653a2984b0eb53a1eadb2523eb3
KatlehoMadaba <101579604+KatlehoMadaba@users.noreply.github.com>
2025-04-13 00:17:08 +0200

    Updated with IUserInterface

---
+++ Frontend/src/providers/auth-provider/models.tsx
@@ -0,0 +1,10 @@
+//User object interface
+export interface IUser {
+    FirstName: string;
+    LastName: string;
+    Email: string;
+    PhoneNumber: string;
+    UserId: string;
+    Role: string;
+  }
+  
