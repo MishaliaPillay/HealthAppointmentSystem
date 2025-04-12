1d41fa26081ff244515ea6b2851907be9ebaffbb
KatlehoMadaba <101579604+KatlehoMadaba@users.noreply.github.com>
2025-04-12 22:34:07 +0200

    Update modes with IProvider entities

---
+++ Frontend/src/providers/providerMedicPrac-provider/models.tsx
@@ -0,0 +1,24 @@
+export interface IProvider {
+    _id:string;
+    FirstName: string;
+    LastName: string;
+    Email: string;
+    Title:string;
+    Biography:string;
+    PhoneNumber:string;
+    YearsOfExperience:number;
+    MaxAppointmentsPerDay:number;
+    Qualification:string;
+}
+export interface IProviderRegisteration {
+    FirstName: string;
+    LastName: string;
+    Email: string;
+    Title:string;
+    Password:string;
+    Biography:string;
+    PhoneNumber:string;
+    YearsOfExperience:number;
+    MaxAppointmentsPerDay:number;
+    Qualification:string;
+}
