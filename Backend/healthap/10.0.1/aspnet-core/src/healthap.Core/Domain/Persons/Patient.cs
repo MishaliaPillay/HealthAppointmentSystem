f25a59e0e50f52ff56a7f02981ad9994fbae83ea
KatlehoMadaba <101579604+KatlehoMadaba@users.noreply.github.com>
2025-04-12 17:24:05 +0200

    Added User Consumption to the entites

--- Backend/healthap/10.0.1/aspnet-core/src/healthap.Core/Domain/Persons/Patient.cs
+++ Backend/healthap/10.0.1/aspnet-core/src/healthap.Core/Domain/Persons/Patient.cs
@@ -1,11 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using Abp.Domain.Entities.Auditing;
+using healthap.Authorization.Users;
+using System.ComponentModel.DataAnnotations.Schema;
 using healthap.Domain.Appointments;
+using System.ComponentModel.DataAnnotations;
 
 namespace healthap.Domain.Persons
 {
-    public class Patient : Person
+    public class Patient : FullAuditedEntity<Guid>
     {
+        [Required]
+        public long UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public virtual User User { get; set; }
+
         public DateTime DateOfBirth { get; set; }
         public string Address { get; set; }
         public string City { get; set; }
@@ -15,4 +25,5 @@ namespace healthap.Domain.Persons
         public virtual ReflistConMethod PreferredContactMedthod { get; set; }
         public virtual ICollection<Appointment> Appointments { get; set; }
     }
+}
 }
--- Backend/healthap/10.0.1/aspnet-core/src/healthap.Core/Domain/Persons/Provider.cs
+++ Backend/healthap/10.0.1/aspnet-core/src/healthap.Core/Domain/Persons/Provider.cs
@@ -1,22 +1,26 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using Abp.Domain.Entities.Auditing;
+using healthap.Authorization.Users;
+using System.ComponentModel.DataAnnotations.Schema;
 using healthap.Domain.Appointments;
+using System.ComponentModel.DataAnnotations;
 
 namespace healthap.Domain.Persons
 {
-    public class Provider : Person
+    public class Provider : FullAuditedEntity<Guid>
     {
+        [Required]
+        public long UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public virtual User User { get; set; }
+
         public string Biography { get; set; }
         public int YearsOfExperience { get; set; }
         public int MaxAppointmentsPerDay { get; set; }
-
-        //TODO:KM cam be a list of type Qualification ?
         public string Qualification { get; set; }
-        //TODO:KM Find out if virtual
-        //public virtual ICollection<Specialty> Specialty { get; set; }
         public virtual ICollection<ProviderAvailabilty>? Availabilities { get; set; } = null;
-        //public virtual ICollection<Locations> Locations { get; set; }
         public virtual ICollection<Appointment>? Appointments { get; set; } = null;
-
-
     }
 }
