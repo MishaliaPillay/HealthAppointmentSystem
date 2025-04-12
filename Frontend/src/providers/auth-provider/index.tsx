e7ed77ad9f246b2d7234b40619801658ef2700ad
KatlehoMadaba <101579604+KatlehoMadaba@users.noreply.github.com>
2025-04-13 01:51:45 +0200

    Changed error handling and added interfaces as error handling

--- Frontend/src/providers/auth-provider/index.tsx
+++ Frontend/src/providers/auth-provider/index.tsx
@@ -1,115 +1,97 @@
-// "use client";
+"use client";
+import { getAxiosInstace } from "@/utils/axios-instance";
+import { IUser, ILoginResponse, ILoginResquest } from "./models";
+import { INITIAL_STATE, UserActionContext, UserStateContext } from "./context";
+import { UserReducer } from "./reducer";
+import {useContext, useReducer } from "react";
+import {
+  getUserError,
+  getUserPending,
+  getUserSuccess,
+  signInError,
+  signInPending,
+  signInSuccess,
+  //   verifyUserPending,
+  //   verifyUserError,
+  //   verifyUserSuccess,
+} from "./actions";
+import axios from "axios";
+import { error } from "console";
+import { isPending } from "@reduxjs/toolkit";
 
-// import {
-//   INITIAL_STATE,
-//   IUser,
-//   UserActionContext,
-//   UserStateContext,
-//   ILoginResponse,
-// } from "./context";
-// import { UserReducer } from "./reducer";
-// import { useContext, useReducer } from "react";
-// import {
-//   getUserError,
-//   getUserPending,
-//   getUserSuccess,
-//   verifyUserPending,
-//   verifyUserError,
-//   verifyUserSuccess,
-// } from "./action";
-// import axios from "axios";
+export const UserProvider = ({ children }: { children: React.ReactNode }) => {
+  const [state, dispatch] = useReducer(UserReducer, INITIAL_STATE);
+  const instance = getAxiosInstace();
 
-// export const UserProvider = ({ children }: { children: React.ReactNode }) => {
-//   const [state, dispatch] = useReducer(UserReducer, INITIAL_STATE);
-//   // const instance = getAxiosInstance();
+  const getUser = async () => {
+    dispatch(getUserPending());
+    const endpoint = `user/current`;
+    const token = sessionStorage.getItem("jwt")?.trim();
+    await instance
+      .get(endpoint, { headers: { Authorization: `${token}` } })
+      .then((response) => {
+        console.log("gettingusers");
+        dispatch(getUserSuccess(response.data.data));
+      })
+      .catch((error) => {
+        console.error(error);
+        dispatch(getUserError());
+        return null; // Return null instead of nothing
+      })
+      .finally(() => {
+        //isPending=false
+        console.log("Done trying to process your getUser request");
+      });
+  };
 
-//   const getUser = async () => {
-//     dispatch(getUserPending());
-//     const endpoint =
-//       "https://body-vault-server-b9ede5286d4c.herokuapp.com/api/user/current";
+  const signIn = async (LoginResquest:ILoginResquest) => {
+    dispatch(signInPending());
+    const endpoint = "users/login";
+    await instance
+      .post<ILoginResponse>(endpoint,LoginResquest)
+      .then((response) => {
+        // console.log("Response", response.data);
+        const token = response.data.token;
+        console.log("session This where token stored");
+        sessionStorage.setItem("jwt", token);
+        dispatch(signInSuccess(response.data));
+        console.log(response);
+      })
+      .catch((error) => {
+        console.error(
+          "Error during login:",
+          error.response?.data?.message || error
+        );
+        dispatch(signInError());
+        throw error;
+      })
+      .finally(() => {
+        //isPending=false
+        console.log("Done trying to process your signIn request");
+      });
+  };
 
-//     try {
-//       const token = sessionStorage.getItem("jwt")?.trim();
+  return (
+    <UserStateContext.Provider value={state}>
+      <UserActionContext.Provider value={{getUser,signIn}}>
+        {children}
+      </UserActionContext.Provider>
+    </UserStateContext.Provider>
+  );
+};
 
-//       console.log("gettingusers");
+export const useUserState = () => {
+  const context = useContext(UserStateContext);
+  if (!context) {
+    throw new Error("useUserState must be used within a UserProvider");
+  }
+  return context;
+};
 
-//       if (!token) {
-//         dispatch(getUserError());
-//         return null; // Return null instead of nothing
-//       }
-
-//       const response = await axios.get(endpoint, {
-//         headers: {
-//           Authorization: `${token}`,
-//         },
-//       });
-
-//       dispatch(getUserSuccess(response.data.data));
-//       return response;
-//     } catch (error) {
-//       console.error(
-//         " Error fetching user details:",
-//         error.response?.data?.message || error
-//       );
-//       dispatch(getUserError());
-//       return null; // Return null instead of nothing
-//     }
-//   };
-
-//   const verifyUser = async (user: IUser) => {
-//     dispatch(verifyUserPending());
-//     const endpoint =
-//       "https://body-vault-server-b9ede5286d4c.herokuapp.com/api/users/login";
-//     try {
-//       console.log("getting User data", user);
-//       const response = await axios.post<ILoginResponse>(endpoint, user);
-//       // console.log("Response", response.data);
-//       const token = response.data.data.token;
-//       if (token) {
-//         console.log("session This where token stored");
-//         sessionStorage.setItem("jwt", token);
-//       } else {
-//         console.error("token not received");
-//       }
-//       //dispatch(getUserSuccess(response.data.data));
-//       dispatch(verifyUserSuccess(response.data));
-//       console.log(response);
-//     } catch (error) {
-//       console.error(
-//         "Error during login:",
-//         error.response?.data?.message || error
-//       );
-//       dispatch(verifyUserError());
-//       throw error;
-//     }
-//   };
-
-//   return (
-//     <UserStateContext.Provider value={state}>
-//       <UserActionContext.Provider
-//         value={{
-//           getUser,
-//           verifyUser,
-//         }}
-//       >
-//         {children}
-//       </UserActionContext.Provider>
-//     </UserStateContext.Provider>
-//   );
-// };
-
-// export const useUserState = () => {
-//   const context = useContext(UserStateContext);
-//   if (!context) {
-//     throw new Error("useUserState must be used within a UserProvider");
-//   }
-//   return context;
-// };
-
-// export const useUserActions = () => {
-//   const context = useContext(UserActionContext);
-//   if (!context) {
-//     throw new Error("useUserActions must be used within a UserProvider");
-//   }
-//   return context;
-// };
+export const useUserActions = () => {
+  const context = useContext(UserActionContext);
+  if (!context) {
+    throw new Error("useUserActions must be used within a UserProvider");
+  }
+  return context;
+};
