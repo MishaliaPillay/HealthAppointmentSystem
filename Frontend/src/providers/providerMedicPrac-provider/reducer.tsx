8ed4901c99f634f4a500c194e8cfd6612b6578f3
KatlehoMadaba <101579604+KatlehoMadaba@users.noreply.github.com>
2025-04-12 22:53:02 +0200

    updated with reducer with provider infomation that is required

---
+++ Frontend/src/providers/providerMedicPrac-provider/reducer.tsx
@@ -0,0 +1,72 @@
+import { handleActions } from "redux-actions";
+import { INITIAL_STATE, IProviderStateContext } from "./context";
+import { ProviderActionEnums } from "./actions";
+
+export const ProviderReducer = handleActions<
+  IProviderStateContext,
+  IProviderStateContext
+>(
+  {
+    [ProviderActionEnums.getProvidersPending]: (state, action) => ({
+      ...state,
+      ...action.payload,
+    }),
+    [ProviderActionEnums.getProvidersSuccess]: (state, action) => ({
+      ...state,
+      ...action.payload,
+    }),
+    [ProviderActionEnums.getProvidersError]: (state, action) => ({
+      ...state,
+      ...action.payload,
+    }),
+    [ProviderActionEnums.getProviderPending]: (state, action) => ({
+      ...state,
+      ...action.payload,
+    }),
+    [ProviderActionEnums.getProviderSuccess]: (state, action) => ({
+      ...state,
+      ...action.payload,
+    }),
+    [ProviderActionEnums.getProviderError]: (state, action) => ({
+      ...state,
+      ...action.payload,
+    }),
+    [ProviderActionEnums.registerProviderPending]: (state, action) => ({
+      ...state,
+      ...action.payload,
+    }),
+    [ProviderActionEnums.registerProviderSuccess]: (state, action) => ({
+      ...state,
+      ...action.payload,
+    }),
+    [ProviderActionEnums.registerProviderError]: (state, action) => ({
+      ...state,
+      ...action.payload,
+    }),
+    [ProviderActionEnums.updateProviderPending]: (state, action) => ({
+      ...state,
+      ...action.payload,
+    }),
+    [ProviderActionEnums.updateProviderSuccess]: (state, action) => ({
+      ...state,
+      ...action.payload,
+    }),
+    [ProviderActionEnums.updateProviderError]: (state, action) => ({
+      ...state,
+      ...action.payload,
+    }),
+    [ProviderActionEnums.deleteProviderPending]: (state, action) => ({
+      ...state,
+      ...action.payload,
+    }),
+    [ProviderActionEnums.deleteProviderSuccess]: (state, action) => ({
+      ...state,
+      ...action.payload,
+    }),
+    [ProviderActionEnums.deleteProviderError]: (state, action) => ({
+      ...state,
+      ...action.payload,
+    }),
+  },
+  INITIAL_STATE
+);
