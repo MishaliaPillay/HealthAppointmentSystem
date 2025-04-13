5a05282d9089e52c71d0801396598cd924bd485d
KatlehoMadaba <101579604+KatlehoMadaba@users.noreply.github.com>
2025-04-13 07:51:11 +0200

    Added Provider and Patient fields to User interface

--- Frontend/src/providers/auth-provider/models.tsx
+++ Frontend/src/providers/auth-provider/models.tsx
@@ -1,15 +1,27 @@
 //User object interface
 export interface IUser {
-    FirstName: string;
-    LastName: string;
-    Email: string;
-    PhoneNumber: string;
-    UserId: string;
-    Role: string;
+    id?:string,
+    title:string,
+    name:string,
+    surname:string, 
+    emailAddress:string,
+    phoneNumber:string,
+    userName:string,
+    password:string,
+    role:string; 
+    dateOfBirth?:Date,
+    address?:string,
+    city?:string,
+    province?:string,
+    postalCode?:string,
+    country?:string,
+    preferredContactMethod?:string
+    Password?:string,
+    Biography?:string,
+    YearsOfExperience?:string,
+    MaxAppointmentsPerDay?:string,
+    Qualification?:string
   }
-  export interface ILoginResponse{
-    token:string;
-}
 export interface ILoginResquest{
     email: string,
     password: string
