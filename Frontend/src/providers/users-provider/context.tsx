3463e35722c9f124ce4da193125b18c6d81f0a56
KatlehoMadaba <101579604+KatlehoMadaba@users.noreply.github.com>
2025-04-13 17:17:17 +0200

    Updated with required interfaces

---
+++ Frontend/src/providers/users-provider/context.tsx
@@ -0,0 +1,37 @@
+"use client";
+import { createContext } from "react";
+import {IUser } from "./models";
+
+
+// Context shape interface
+export interface IUserStateContext {
+  isPending: boolean;
+  isSuccess: boolean;
+  isError: boolean;
+  user?: IUser;
+  users?: IUser[]; // Array of Users
+}
+
+// User action context interface
+export interface IUserActionContext {
+  getCurrentUser: () => Promise<void>;
+  getUsers: () => void; // Fetch all Users
+  getUser: (id: string) => void; // Fetch a single User
+  createUser: (user: IUser) => void; // Create a new User
+  updateUser: (user: IUser) => void; // Update existing User
+  deleteUser: (id: string) => void; // Delete a user
+}
+
+// Initial state with default values
+export const INITIAL_STATE: IUserStateContext = {
+  isPending: false,
+  isSuccess: false,
+  isError: false,
+  users:[],
+};
+
+// Create the state context and the action context
+export const UserStateContext = createContext<IUserStateContext>(INITIAL_STATE);
+export const UserActionContext = createContext<IUserActionContext | undefined>(
+  undefined
+);
