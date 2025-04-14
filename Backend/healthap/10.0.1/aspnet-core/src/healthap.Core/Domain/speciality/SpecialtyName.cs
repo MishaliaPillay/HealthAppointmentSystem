42cbd9a56a114e01e19b84a27f7e7a916b74ed27
Tondwani <craigmangaladzi@gmail.com>
2025-04-14 20:08:18 +0200

    Added migration to update the database schema with Speciality table

---
+++ Backend/healthap/10.0.1/aspnet-core/src/healthap.Core/Domain/speciality/SpecialtyName.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace healthap.MultiTenancy.speciality
+{
+    public enum SpecialtyName
+    {
+        Cardiology,
+        Dermatology,
+        FamilyMedicine,
+        Gastroenterology,
+        InternalMedicine,
+        Neurology,
+        Obstetrics,
+        Oncology,
+        Ophthalmology,
+        Orthopedics,
+        Pediatrics,
+        Psychiatry,
+        Radiology,
+        Urology
+
+    }
+}
---
+++ Backend/healthap/10.0.1/aspnet-core/src/healthap.Core/Domain/speciality/speciality.cs
@@ -0,0 +1,20 @@
+﻿using Abp.Domain.Entities;
+using healthap.Domain.Persons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace healthap.MultiTenancy.speciality
+{
+    public class Speciality : Entity
+    {
+        public SpecialtyName SpecialtyName { get; set; }
+        public Guid ProviderId { get; set; }
+
+        // Navigation property
+        public virtual Provider Provider { get; set; }
+
+    }
+}
