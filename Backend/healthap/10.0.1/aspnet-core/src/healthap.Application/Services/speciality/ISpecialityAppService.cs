aae8662b57bc8b30a4fe6c09add2e4ca79aa6ccd
Tondwani <craigmangaladzi@gmail.com>
2025-04-14 20:32:22 +0200

    Testing speciality endpoints

---
+++ Backend/healthap/10.0.1/aspnet-core/src/healthap.Application/Services/speciality/ISpecialityAppService.cs
@@ -0,0 +1,15 @@
+﻿using Abp.Application.Services;
+using healthap.Services.speciality.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace healthap.Services.speciality
+{
+    public interface ISpecialityAppService : IApplicationService
+    {
+        Task<List<string>> GetAllSpecialitiesAsync();
+    }
+}
---
+++ Backend/healthap/10.0.1/aspnet-core/src/healthap.Application/Services/speciality/SpecialityAppService.cs
@@ -0,0 +1,22 @@
+﻿using Abp.Application.Services;
+using Abp.Domain.Repositories;
+using AutoMapper.Internal.Mappers;
+using healthap.MultiTenancy.speciality;
+using healthap.Services.speciality.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace healthap.Services.speciality
+{
+    public class SpecialityAppService : ApplicationService, ISpecialityAppService
+    {
+        public async Task<List<string>> GetAllSpecialitiesAsync()
+        {
+            var specialties = Enum.GetNames(typeof(SpecialtyName)).ToList();
+            return await Task.FromResult(specialties);
+        }
+    }
+}
