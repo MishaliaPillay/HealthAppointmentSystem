4c2a8128fa65c5fdf6fa18892b2b4a2fe6121330
MatimaMk <matima547@gmail.com>
2025-04-16 00:08:30 +0200

    institution Location Provider

---
+++ Frontend/src/providers/institutionLocation-provider/reducer.tsx
@@ -0,0 +1,42 @@
+"use client";
+import { LocationActionEnums } from "./actions";
+import { ILocationStateContext, INITIAL_STATE } from "./context";
+import { handleActions } from "redux-actions";
+
+export const LocationReducer = handleActions<
+  ILocationStateContext,
+  ILocationStateContext
+>(
+  {
+    // get places by description
+    [LocationActionEnums.getPlacesByDescriptionPending]: (state, action) => ({
+      ...state,
+      ...action.payload,
+    }),
+    [LocationActionEnums.getPlacesByDescriptionSuccess]: (state, action) => ({
+      ...state,
+      ...action.payload,
+    }),
+
+    [LocationActionEnums.getPlacesByDescriptionError]: (state, action) => ({
+      ...state,
+      ...action.payload,
+    }),
+
+    //get places by state
+    [LocationActionEnums.getPlacesByStatePending]: (state, action) => ({
+      ...state,
+      ...action.payload,
+    }),
+    [LocationActionEnums.getPlacesByStateSuccess]: (state, action) => ({
+      ...state,
+      ...action.payload,
+    }),
+
+    [LocationActionEnums.getPlacesByStateError]: (state, action) => ({
+      ...state,
+      ...action.payload,
+    }),
+  },
+  INITIAL_STATE
+);
