6119242a17f405292a175fd95c6ed6d9d8301aa4
KatlehoMadaba <101579604+KatlehoMadaba@users.noreply.github.com>
2025-04-15 02:58:58 +0200

    added fetch user method

--- Frontend/src/app/patient-dashboard/profile/page.tsx
+++ Frontend/src/app/patient-dashboard/profile/page.tsx
@@ -1,19 +1,47 @@
-'use client';
+"use client";
 
-import { Typography, Form, Input, Button, Card, Avatar, Row, Col } from 'antd';
-import { UserOutlined } from '@ant-design/icons';
-
+import { Typography, Form, Input, Button, Card, Avatar, Row, Col } from "antd";
+import { UserOutlined } from "@ant-design/icons";
+import { useEffect, useState } from "react";
+import { getCurrentUserError } from "@/providers/users-provider/actions";
+import { useUserActions } from "../../../providers/users-provider";
+import { useUserState } from "../../../providers/users-provider";
+import { isPending } from "@reduxjs/toolkit";
+import { IUser } from "@/providers/users-provider/models";
+import { getuid } from "process";
 const { Title } = Typography;
 
 export default function ProfilePage() {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [Loader, setLoader] = useState(true);
+  const { getCurrentUser, getUser } = useUserActions();
+  const { isPending } = useUserState();
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    setToken(sessionStorage.getItem("jwtToken"));
+  }, []);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      if (token) {
+        const user = await getCurrentUser();
+        if (user) {
+          console.log("effect", user);
+          getUser(user.id);
+        }
+      }
+    };
+    fetchUser();
+  }, [token]);
+
   return (
     <div>
       <Title level={2}>Profile</Title>
-      
       <Card>
         <Row gutter={[24, 24]}>
           <Col xs={24} md={8}>
-            <div style={{ textAlign: 'center' }}>
+            <div style={{ textAlign: "center" }}>
               <Avatar size={100} icon={<UserOutlined />} />
               <div style={{ marginTop: 16 }}>
                 <Button type="primary">Change Avatar</Button>
@@ -25,7 +53,11 @@ export default function ProfilePage() {
               <Form.Item label="Full Name" name="name" initialValue="John Doe">
                 <Input />
               </Form.Item>
-              <Form.Item label="Email" name="email" initialValue="john.doe@example.com">
+              <Form.Item
+                label="Email"
+                name="email"
+                initialValue="john.doe@example.com"
+              >
                 <Input />
               </Form.Item>
               <Form.Item label="Phone" name="phone" initialValue="515151454513">
