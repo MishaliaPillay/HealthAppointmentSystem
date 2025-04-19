3494055ba5577670e9a9686fdfe1362c63fd78de
KatlehoMadaba <101579604+KatlehoMadaba@users.noreply.github.com>
2025-04-19 18:55:22 +0200

    inherited the userrequestDto

--- Backend/healthap/10.0.1/aspnet-core/src/healthap.Application/Services/PersonServices/Dtos/UpdateProviderDto.cs
+++ Backend/healthap/10.0.1/aspnet-core/src/healthap.Application/Services/PersonServices/Dtos/UpdateProviderDto.cs
@@ -6,14 +6,9 @@ using System.Threading.Tasks;
 
 namespace healthap.Services.PersonServices.Dtos
 {
-    public class UpdateProviderDto
+    public class UpdateProviderDto: UserRequestDto
     {
         public Guid ProviderId { get; set; }
-        public string? Name { get; set; }
-        public string? Surname { get; set; }
-        public string? EmailAddress { get; set; }
-        public string? UserName { get; set; }
-        public string? Password { get; set; }
         public string? Title { get; set; }
         public string? PhoneNumber { get; set; }
         public string? Biography { get; set; }
