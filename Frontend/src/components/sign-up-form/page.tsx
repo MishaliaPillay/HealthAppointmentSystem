b71aef975bc06f8399ce02194334aa2ff8756699
MishaliaPillay <117475572+MishaliaPillay@users.noreply.github.com>
2025-04-24 09:35:08 +0200

    Update page.tsx

--- Frontend/src/components/sign-up-form/page.tsx
+++ Frontend/src/components/sign-up-form/page.tsx
@@ -464,17 +464,14 @@ export default function SignupForm({ onBeforeSubmit }: SignupFormProps) {
                       .includes(input.toLowerCase())
                   }
                   options={institutions.map((inst) => ({
-                    label: inst.description,
-                    value: inst.id,
+                    value: inst.institutionId,
+                    label: inst.institutionName, // ✅ added label here
                     inst,
                   }))}
                   optionRender={(option) => {
                     const inst = option.data.inst;
                     return (
                       <div className={styles.institutionOption}>
-                        <span className={styles.institutionName}>
-                          {inst.description}
-                        </span>
                         <span className={styles.institutionAddress}>
                           {inst.address} • {inst.city}, {inst.state}
                         </span>
